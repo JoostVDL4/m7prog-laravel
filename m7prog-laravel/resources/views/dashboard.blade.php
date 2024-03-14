55559c2d41dc873aa44c6e248dd0fa59a73c2418
Joost <35542@ma-web.nl>
2024-03-14 12:01:48 +0100

    layout

--- m7prog-laravel/resources/views/dashboard.blade.php
+++ m7prog-laravel/resources/views/dashboard.blade.php
@@ -1,5 +1,47 @@
 <x-app-layout>
-<html>
- 
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <header class="bg-gray-800 text-white py-4">
+            <div class="container mx-auto flex justify-between items-center px-4">
+                <h1 class="text-xl font-semibold">Portfolio</h1>
+                <nav>
+                    <ul class="flex space-x-4">
+                        <li><a href="#" class="hover:text-gray-300"> Home</a></li>
+                        <li><a href="#" class="hover:text-gray-300"> About</a></li>
+                        <li><a href="#" class="hover:text-gray-300"> blah blah</a></li>
+                        <li><a href="#" class="hover:text-gray-300"> Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+
+        <section>
+            <div>
+                <p class= "text-white flex justify-center ">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                     Possimus quisquam facere odit repudiandae doloribus cumque in non fugit eum dolorem iusto,
+                      ab asperiores rem aliquam eveniet molestiae atque, suscipit nobis!</p>
+            </div>
+        </section>
+
+
+        <footer class="bg-gray-800 text-white py-4">
+            <div class="container mx-auto flex justify-between items-center px-4">
+                <h1 class="text-xl font-semibold">Portfolio</h1>
+                <nav>
+                    <ul class="flex space-x-4">
+                        <li><a href="#" class="hover:text-gray-300"> Copyright</a></li>
+                
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    </body>
+    </html>
 </x-app-layout>
